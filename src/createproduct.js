3260d511a02a0dd316b64babb07d0f9c5e50c7db
Dhinesh Dhamodharan <dhineshdhamodharan@gmail.com>
2021-09-05 16:41:27 +0530

    Updated

--- src/createproduct.js
+++ src/createproduct.js
@@ -27,12 +27,13 @@ function Createproduct(props) {
       const errors = {};
       if (!values.productName) {
         errors.productName = "Required";
-      } else if (values.productName.length > 25) {
+      }else if(values.productName.length>25)
+      {
         errors.productName = "Please enter Product Name with in 25 character";
       }
       if (!values.price) {
         errors.price = "Required";
-      } else if (!Number(values.price)) {
+      }else if(!Number(values.price)){
         errors.price = "Please enter the price in Numbers";
       }
       if (!values.manufactureDate) {
@@ -43,20 +44,21 @@ function Createproduct(props) {
       }
       if (!values.productType) {
         errors.productType = "Required";
-      } else if (Number(values.productType)) {
+      }else if(Number(values.productType))
+      {
         errors.productType = "Please enter the Product type in Alphabets";
       }
       return errors;
     },
-    onSubmit: async (values) => {
+    onSubmit:async(values)=>{
       try {
         setLoading(true);
-        await axios.post("http://localhost:3000/create-products", {
-          productName: values.productName,
-          price: values.price,
-          manufactureDate: values.manufactureDate,
-          expirDate: values.expirDate,
-          productType: values.productType,
+        await axios.post("https://60f1550c38ecdf0017b0fbac.mockapi.io/Product", {
+          productName:values.productName,
+          price:values.price,
+          manufactureDate:values.manufactureDate,
+          expirDate:values.expirDate,
+          productType:values.productType
         });
         setLoading(false);
         history.push("/products");
@@ -64,7 +66,7 @@ function Createproduct(props) {
         console.log("Error");
         setLoading(false);
       }
-    },
+    }
   });
 
   return (
@@ -85,9 +87,7 @@ function Createproduct(props) {
                 className="form-control"
               />
               {formik.errors.productName ? (
-                <span style={{ color: "red" }}>
-                  {formik.errors.productName}
-                </span>
+                <span style={{ color: "red" }}>{formik.errors.productName}</span>
               ) : null}
               <br />
             </div>
@@ -143,9 +143,7 @@ function Createproduct(props) {
                 className="form-control"
               />
               {formik.errors.productType ? (
-                <span style={{ color: "red" }}>
-                  {formik.errors.productType}
-                </span>
+                <span style={{ color: "red" }}>{formik.errors.productType}</span>
               ) : null}
               <br />
             </div>
